d65781be7e20889d0cdc9dd009fda7e39a868e66
James Michael <jomichael1994@gmail.com>
2025-10-01 16:44:24 +0100

    refactor footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -8,48 +8,55 @@ import Logo from './Logo';
 
 const Footer = () => {
 	return (
-		<div className="grid grid-rows-2 gap-8 sm:gap-0 sm:grid-rows-none sm:grid-cols-2 pb-44 py-10 bg-black">
-			<div className="grid grid-rows-2 sm:h-full m-auto">
+		<div className="grid grid-rows-[auto_auto] gap-6 md:gap-0 pb-44 py-10 xl:max-w-screen-xl 2xl:max-w-screen-2xl px-8 md:px-14 2xl:px-8 mx-auto">
+			<div className="grid grid-cols-none grid-rows-2 md:grid-rows-none md:grid-cols-2 gap-6 md:gap-44">
 				<Logo
-					className="h-10 text-lg flex justify-center sm:justify-start items-center"
+					className="h-10 text-lg lg:text-xl flex justify-center md:justify-start items-center"
 					light={true}
 				/>
-				<span className="font-unbounded text-xs text-neutral-300 flex justify-center sm:justify-start items-center">
-					&copy; James Michael, {new Date().getFullYear()}
-				</span>
-			</div>
-			<div className="grid grid-rows-2 sm:h-full m-auto">
-				<div className="h-10 flex gap-10 text-neutral-300 justify-center sm:justify-end items-center">
+				<div className="h-10 flex gap-8 sm:gap-10 text-neutral-500 justify-center md:justify-end items-center">
+					<Link
+						href="https://jamesmichael.dev"
+						title="Portfolio '25"
+						className="group font-georama text-2xl sm:text-[1.8rem]"
+						target="_blank"
+					>
+						<span className="transition-all duration-150 group-hover:text-white font-stretch-110% font-extralight">
+							J
+						</span>
+						<span className="transition-all duration-150 group-hover:text-orange-200 font-stretch-[130%] font-extrabold">
+							M
+						</span>
+					</Link>
 					<Link
 						href="https://github.com/jamesomichael"
 						target="_blank"
-						className="hover:text-spotify-green"
+						title="GitHub"
+						className="transition-all duration-150 hover:text-white"
 					>
 						<FaGithub size={30} />
 					</Link>
 					<Link
 						href="https://linkedin.com/in/jamesm94"
 						target="_blank"
-						className="hover:text-spotify-green"
+						title="LinkedIn"
+						className="transition-all duration-150 hover:text-white"
 					>
 						<FaLinkedin size={30} />
 					</Link>
 					<Link
 						href="mailto:hello@jamesmichael.dev"
-						className="hover:text-spotify-green"
+						title="Email"
+						className="transition-all duration-150 hover:text-white"
 					>
 						<IoMdMail size={30} />
 					</Link>
 				</div>
-				<div className="font-unbounded text-neutral-300 text-xs flex justify-center sm:justify-end items-center">
-					<Link
-						href="https://jamesmichael.dev"
-						target="_blank"
-						className="hover:text-spotify-green"
-					>
-						jamesmichael.dev
-					</Link>
-				</div>
+			</div>
+			<div className="h-fit flex justify-center md:justify-start items-center">
+				<span className="py-2 md:py-6 font-funnel text-xs sm:text-sm text-neutral-500">
+					&copy; James Michael, {new Date().getFullYear()}
+				</span>
 			</div>
 		</div>
 	);

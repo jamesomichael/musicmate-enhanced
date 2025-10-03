b5e9c6186463e929d811ac7664f9ad97dab55ee7
James Michael <jomichael1994@gmail.com>
2025-10-03 15:08:33 +0100

    update redux config to support preloaded state

--- redux/StoreProvider.tsx
+++ redux/StoreProvider.tsx
@@ -1,16 +1,20 @@
 'use client';
 import { useRef } from 'react';
 import { Provider } from 'react-redux';
-import { makeStore, AppStore } from './store';
+import { PreloadedStateShapeFromReducersMapObject } from '@reduxjs/toolkit';
+
+import { makeStore, AppStore, RootState } from './store';
 
 export default function StoreProvider({
 	children,
+	preloadedState,
 }: {
 	children: React.ReactNode;
+	preloadedState?: PreloadedStateShapeFromReducersMapObject<RootState>;
 }) {
 	const storeRef = useRef<AppStore | null>(null);
 	if (!storeRef.current) {
-		storeRef.current = makeStore();
+		storeRef.current = makeStore(preloadedState);
 	}
 
 	return <Provider store={storeRef.current}>{children}</Provider>;
--- redux/store.ts
+++ redux/store.ts
@@ -1,15 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+	configureStore,
+	StateFromReducersMapObject,
+	PreloadedStateShapeFromReducersMapObject,
+} from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
+import userReducer from './slices/userSlice';
 
-export const makeStore = () => {
-	return configureStore({
-		reducer: {
-			auth: authReducer,
-		},
-	});
+const reducer = {
+	auth: authReducer,
+	user: userReducer,
 };
 
 export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore['getState']>;
+export type RootState = StateFromReducersMapObject<typeof reducer>;
+
+export const makeStore = (
+	preloadedState?: PreloadedStateShapeFromReducersMapObject<RootState>
+) => {
+	return configureStore({
+		reducer,
+		preloadedState,
+	});
+};
+
 export type AppDispatch = AppStore['dispatch'];

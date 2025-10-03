a69c2b066886c2efb3203eecce275e68125e15d7
James Michael <jomichael1994@gmail.com>
2025-10-03 19:21:33 +0100

    add and integrate navbar

---
+++ components/navbar/Navbar/index.tsx
@@ -0,0 +1,38 @@
+'use client';
+import React from 'react';
+
+import Navigation from '../Navigation';
+import Search from '../Search';
+import NavButton from '../NavButton';
+
+import { GoHomeFill, GoHome } from 'react-icons/go';
+import { BiSolidCategory, BiCategory } from 'react-icons/bi';
+
+const Navbar = () => {
+	return (
+		<div className="px-2 py-1.5 h-full grid grid-cols-[1fr_2fr_1fr] items-center">
+			<div>
+				<Navigation />
+			</div>
+			<div className="h-full flex gap-1.5 justify-center items-center">
+				<NavButton
+					href="/"
+					title="Home"
+					ActiveIcon={GoHomeFill}
+					InactiveIcon={GoHome}
+					iconSize={22}
+				/>
+				<Search />
+				<NavButton
+					href="/browse"
+					title="Browse"
+					ActiveIcon={BiSolidCategory}
+					InactiveIcon={BiCategory}
+					iconSize={22}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
--- layouts/AppLayout/DesktopLayout.tsx
+++ layouts/AppLayout/DesktopLayout.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import Navbar from '@/components/navbar/Navbar';
+
 const DesktopLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="flex flex-col h-full">
 			<div className="h-14">
-				<div className="h-full bg-blue-200">NAVBAR</div>
+				<Navbar />
 			</div>
-			<div className="flex-1 p-2 h-full overflow-hidden">
+			<div className="flex-1 px-2 pb-2 h-full overflow-hidden">
 				<div className="h-full grid grid-cols-[auto_1fr] gap-2">
 					<div className="bg-yellow-400 w-72 rounded-md">
 						LIBRARY PANEL

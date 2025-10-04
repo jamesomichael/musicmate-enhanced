1f49472744fb3d76d8c0daffdd3999b833b3fa7e
James Michael <jomichael1994@gmail.com>
2025-10-04 02:17:44 +0100

    add background images

--- constants/backgrounds.ts
+++ constants/backgrounds.ts
@@ -5,6 +5,7 @@ export const BACKGROUND_IMAGES = [
 			name: 'Glenn van de Wiel',
 			url: 'https://unsplash.com/@glennvandewiel?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
 		},
+		opacity: 'bg-black/30 sm:bg-black/25',
 	},
 	{
 		url: 'https://images.unsplash.com/photo-1522717887140-b78153b59e52?q=80&w=3552&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -12,6 +13,7 @@ export const BACKGROUND_IMAGES = [
 			name: 'Shane Rounce',
 			url: 'https://unsplash.com/@shanerounce?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
 		},
+		opacity: 'bg-black/25',
 	},
 	{
 		url: 'https://images.unsplash.com/photo-1604514288114-3851479df2f2?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -27,7 +29,7 @@ export const BACKGROUND_IMAGES = [
 			name: 'Patrick Hodskins',
 			url: 'https://unsplash.com/@phodskins?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
 		},
-		opacity: 'bg-black/40 sm:bg-black/15',
+		opacity: 'bg-black/35 sm:bg-black/15',
 	},
 	{
 		url: 'https://images.unsplash.com/photo-1614247912229-26a7e2114c0a?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -52,5 +54,77 @@ export const BACKGROUND_IMAGES = [
 			url: 'https://unsplash.com/@bhurnal?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
 		},
 		opacity: 'bg-black/40 sm:bg-black/35',
+	},
+	{
+		url: 'https://images.unsplash.com/photo-1483101974978-cf266fdf1139?q=80&w=4032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		attribution: {
+			name: 'Diego Sulivan',
+			url: 'https://unsplash.com/@sulivan?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+		},
+		opacity: 'bg-black/10 sm:bg-black/5',
+	},
+	{
+		url: 'https://images.unsplash.com/photo-1583795310794-4168d4ff1be2?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		attribution: {
+			name: 'Spencer Davis',
+			url: 'https://unsplash.com/@spencerdavis?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+		},
+		opacity: 'bg-black/45 sm:bg-black/20',
+	},
+	{
+		url: 'https://images.unsplash.com/photo-1628144807516-d75faf7a0747?q=80&w=3546&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		attribution: {
+			name: 'Joseph Lee',
+			url: 'https://unsplash.com/@jelphoto?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+		},
+		opacity: 'bg-black/15 sm:bg-black/0',
+	},
+	{
+		url: 'https://images.unsplash.com/photo-1575672913784-11a7cd4f25f4?q=80&w=3538&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		attribution: {
+			name: 'Natalie Parham',
+			url: 'https://unsplash.com/@natalieparham?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+		},
+		opacity: 'bg-black/20 sm:bg-black/30',
+	},
+	{
+		url: 'https://images.unsplash.com/photo-1722505531623-7b43ba7319f8?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		attribution: {
+			name: 'Frederick Shaw',
+			url: 'https://unsplash.com/@dropfastcollective?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+		},
+		opacity: 'bg-black/30 sm:bg-black/0',
+	},
+	{
+		url: 'https://images.unsplash.com/photo-1556848797-e26fc1b2d6cc?q=80&w=4031&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		attribution: {
+			name: 'Nicholas Jeffries',
+			url: 'https://unsplash.com/@nickleejeffries?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+		},
+		opacity: 'bg-black/0',
+	},
+	{
+		url: 'https://images.unsplash.com/photo-1450044804117-534ccd6e6a3a?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		attribution: {
+			name: 'Abigail Lynn',
+			url: 'https://unsplash.com/@shmabbss?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+		},
+		opacity: 'bg-black/25 sm:bg-black/40',
+	},
+	{
+		url: 'https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		attribution: {
+			name: 'Yvette de Wit',
+			url: 'https://unsplash.com/@yvettedewit?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+		},
+		opacity: 'bg-black/40 sm:bg-black/15',
+	},
+	{
+		url: 'https://images.unsplash.com/photo-1510682657356-6ee07db8204b?q=80&w=4032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		attribution: {
+			name: 'Dominic Hampton',
+			url: 'https://unsplash.com/@dhamp15?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash',
+		},
+		opacity: 'bg-black/40 sm:bg-black/20',
 	},
 ];

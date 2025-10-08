c47c7bcb5ffac5195935fbfccc53c9b56766e656
James Michael <jomichael1994@gmail.com>
2025-10-08 23:57:55 +0100

    update NowPlaying to use selector, check for podcast

--- components/player/NowPlaying/index.tsx
+++ components/player/NowPlaying/index.tsx
@@ -1,31 +1,50 @@
 import React from 'react';
+import Link from 'next/link';
 
 import { useAppSelector } from '@/redux/hooks';
-import { getCurrentTrack } from '@/redux/slices/playerSlice';
+import { getNowPlaying } from '@/redux/slices/playerSlice';
 
 const NowPlaying = () => {
-	const currentTrack = useAppSelector(getCurrentTrack);
-
-	if (!currentTrack) {
-		return null;
-	}
+	const { isPodcast = false, item } = useAppSelector(getNowPlaying);
 
 	return (
-		<div className="flex items-center gap-3 overflow-hidden py-3">
-			<div
-				className="h-full bg-cover bg-center aspect-square bg-black rounded-md"
-				style={{
-					backgroundImage: `url(${currentTrack.album.images[0].url})`,
-				}}
-			></div>
-			<div className="flex flex-col truncate">
-				<span className="leading-5 truncate font-funnel font-medium text-white text-base">
-					{currentTrack.name}
+		<div className="flex items-center gap-3 overflow-hidden py-3 pr-8">
+			{isPodcast ? (
+				<span className="font-funnel font-medium text-white">
+					Podcasts are not currently supported.
 				</span>
-				<span className="font-funnel truncate text-neutral-400 text-sm">
-					{currentTrack.artists[0].name}
-				</span>
-			</div>
+			) : (
+				<>
+					<div
+						className="h-full bg-cover bg-center aspect-square bg-black rounded-md"
+						style={{
+							backgroundImage: `url(${item.album.images[0].url})`,
+						}}
+					></div>
+					<div className="flex flex-col overflow-hidden">
+						<span className="leading-5 truncate font-funnel font-medium text-white text-base">
+							{item.name}
+						</span>
+
+						<div className="truncate overflow-hidden w-full">
+							{item.artists.map((artist, idx: number) => (
+								<span
+									key={artist.id}
+									className="font-funnel text-neutral-400 text-sm"
+								>
+									<Link
+										href={`/artists/${artist.id}`}
+										className="hover:text-white hover:underline"
+									>
+										{artist.name}
+									</Link>
+									{idx < item.artists.length - 1 && ', '}
+								</span>
+							))}
+						</div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };

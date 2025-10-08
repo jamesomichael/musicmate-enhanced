0e6808a3a3ca9237c2613ab5e32e698234f43fd9
James Michael <jomichael1994@gmail.com>
2025-10-08 23:57:19 +0100

    add feature icons to PlayerExtras

--- components/player/PlayerExtras/index.tsx
+++ components/player/PlayerExtras/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import { FaHeadphones } from 'react-icons/fa6';
+import { HiOutlineQueueList } from 'react-icons/hi2';
+
 import VolumeControl from '../VolumeControl';
 
 const PlayerExtras = () => {
 	return (
-		<div className="flex justify-end items-center">
+		<div className="flex justify-end items-center gap-5 pr-1">
+			<div title="Queue">
+				<HiOutlineQueueList
+					className="transition-all duration-200 cursor-pointer text-neutral-300 hover:scale-105 hover:text-white"
+					size={21}
+				/>
+			</div>
+			<div title="Connect to a device">
+				<FaHeadphones
+					className="transition-all duration-200 cursor-pointer text-neutral-300 hover:scale-105 hover:text-white"
+					size={17}
+				/>
+			</div>
 			<VolumeControl />
 		</div>
 	);

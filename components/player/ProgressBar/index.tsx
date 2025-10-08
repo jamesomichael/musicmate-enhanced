c0c5d397433f8923df227208aef252362f6f47e7
James Michael <jomichael1994@gmail.com>
2025-10-08 23:56:36 +0100

    integrate progress bar

--- components/player/ProgressBar/index.tsx
+++ components/player/ProgressBar/index.tsx
@@ -1,9 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+
+dayjs.extend(duration);
 
 import SeekBar from '@/components/shared/SeekBar';
 
+import { useAppSelector } from '@/redux/hooks';
+import { getNowPlaying } from '@/redux/slices/playerSlice';
+
 const ProgressBar = () => {
-	const [position, setPosition] = useState(60);
+	const {
+		isPlaying,
+		isExternal,
+		progress,
+		item: { duration_ms: duration },
+	} = useAppSelector(getNowPlaying);
+
+	const [position, setPosition] = useState<number>(progress);
+
+	useEffect(() => {
+		setPosition(progress);
+	}, [progress]);
+
+	useEffect(() => {
+		if (!isPlaying || !isExternal) {
+			return;
+		}
+
+		const interval = setInterval(() => {
+			setPosition((prev) => {
+				if (prev + 1000 >= duration) {
+					clearInterval(interval);
+					return duration;
+				}
+				return prev + 1000;
+			});
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, [isPlaying, duration]);
 
 	const handleSeek = (value: number) => {
 		setPosition(value);
@@ -12,13 +48,15 @@ const ProgressBar = () => {
 	return (
 		<div className="flex justify-center items-center gap-2 w-full">
 			<span className="font-funnel text-xs text-neutral-300">
-				1:00
-				{/* {dayjs.duration(progress, 'milliseconds').format('m:ss')} */}
+				{dayjs.duration(position, 'milliseconds').format('m:ss')}
 			</span>
-			<SeekBar duration={180} position={position} onSeek={handleSeek} />
+			<SeekBar
+				duration={duration}
+				position={position}
+				onSeek={handleSeek}
+			/>
 			<span className="font-funnel text-xs text-neutral-300">
-				3:00
-				{/* {dayjs.duration(progress, 'milliseconds').format('m:ss')} */}
+				{dayjs.duration(duration, 'milliseconds').format('m:ss')}
 			</span>
 		</div>
 	);

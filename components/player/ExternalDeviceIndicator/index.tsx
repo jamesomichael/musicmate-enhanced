73e608dc1c442b6c1a06ed6cb672b6b3afbacbe5
James Michael <jomichael1994@gmail.com>
2025-10-08 23:58:30 +0100

    integrate ExternalDeviceIndicator component

--- components/player/ExternalDeviceIndicator/index.tsx
+++ components/player/ExternalDeviceIndicator/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import { FaVolumeHigh } from 'react-icons/fa6';
 
+import { useAppSelector } from '@/redux/hooks';
+import { getNowPlaying } from '@/redux/slices/playerSlice';
+
 const ExternalDeviceIndicator = () => {
+	const { device } = useAppSelector(getNowPlaying);
+
 	return (
 		<div className="px-2 bg-spotify-green h-6 flex gap-2 justify-end items-center rounded">
 			<FaVolumeHigh />
 			<span className="font-funnel font-bold text-sm">
-				Playing on TEST
+				Playing on {device.name}
 			</span>
 		</div>
 	);

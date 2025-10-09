ed7395bcf728b1560fad7c471508b59d201ee327
James Michael <jomichael1994@gmail.com>
2025-10-09 23:58:33 +0100

    wip library panel components

---
+++ components/library/LibraryPanel/ListItem/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { BsFillPinAngleFill } from 'react-icons/bs';
+
+const ListItem = ({
+	href,
+	imageUrl,
+	name,
+	isPinned,
+	secondaryText,
+}: {
+	href: string;
+	imageUrl?: string;
+	name: string;
+	isPinned?: boolean;
+	secondaryText?: string;
+}) => {
+	const pathname = usePathname();
+	return (
+		<Link
+			href={href}
+			className={`${
+				href === pathname
+					? 'bg-[#ffffff1a] hover:bg-neutral-700'
+					: 'hover:bg-neutral-800'
+			} transition-all duration-200 hover:cursor-pointer flex items-center gap-2 p-2 h-16 rounded-md`}
+		>
+			{imageUrl && (
+				<div
+					className="h-full aspect-square rounded-md bg-cover bg-center"
+					style={{
+						backgroundImage: `url(${imageUrl})`,
+					}}
+				></div>
+			)}
+			<div className="flex flex-col justify-center truncate">
+				<div className="flex items-center gap-1">
+					{isPinned && (
+						<BsFillPinAngleFill className="text-spotify-green" />
+					)}
+					<span className="font-funnel truncate">{name}</span>
+				</div>
+				{secondaryText && (
+					<span className="truncate font-funnel text-sm text-neutral-400">
+						{secondaryText}
+					</span>
+				)}
+			</div>
+		</Link>
+	);
+};
+
+export default ListItem;
--- components/library/LibraryPanel/index.tsx
+++ components/library/LibraryPanel/index.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
+import Link from 'next/link';
 
 import { LuSquareLibrary } from 'react-icons/lu';
+import { BsFillPinAngleFill } from 'react-icons/bs';
 
 import LibraryTabs from './LibraryTabs';
+import Loader from '@/components/shared/Loader';
+
+import { useAppSelector } from '@/redux/hooks';
+import { useAppDispatch } from '@/redux/hooks';
+import { fetchUserPlaylists } from '@/redux/slices/librarySlice';
+import ListItem from './ListItem';
 
 const LibraryPanel = () => {
+	const { playlists } = useAppSelector((state) => state.library);
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(fetchUserPlaylists());
+	}, []);
+
 	return (
-		<div className="flex flex-col gap-4 p-3">
-			<div className="flex items-center gap-2 text-neutral-300">
-				<LuSquareLibrary size={25} />
-				<span className="font-funnel text-lg font-bold">
-					Your Library
-				</span>
+		<div className="h-full flex flex-col gap-2 p-2 overflow-auto">
+			<div className="flex flex-col gap-4 p-2">
+				<div className="flex items-center py-1 gap-2 text-neutral-300">
+					<LuSquareLibrary size={25} />
+					<span className="font-funnel text-lg font-bold">
+						Your Library
+					</span>
+				</div>
+				<LibraryTabs />
 			</div>
-			<LibraryTabs />
+			<div className="h-full text-white">
+				{playlists.isLoading ? (
+					<Loader />
+				) : (
+					<div className="flex flex-col pb-3">
+						{playlists.items.map((playlist) => (
+							<ListItem
+								key={playlist.id}
+								href={`/playlist/${playlist.id}`}
+								imageUrl={playlist.images[0]?.url}
+								name={playlist.name}
+								isPinned={playlist.isPinned}
+								secondaryText={playlist.owner?.display_name}
+							/>
+						))}
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };

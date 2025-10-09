5b51533bee8d442a67a567e5eeab158a11992320
James Michael <jomichael1994@gmail.com>
2025-10-09 15:45:10 +0100

    update desktop layout to use library panel

--- layouts/AppLayout/DesktopLayout.tsx
+++ layouts/AppLayout/DesktopLayout.tsx
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers';
 
 import Navbar from '@/components/navbar/Navbar';
 import PlayerContainer from '@/components/player/PlayerContainer';
+import LibraryPanel from '@/components/library/LibraryPanel';
 
 const DesktopLayout = async ({ children }: { children: React.ReactNode }) => {
 	const cookieStore = await cookies();
@@ -14,8 +15,8 @@ const DesktopLayout = async ({ children }: { children: React.ReactNode }) => {
 			</div>
 			<div className="flex-1 px-2 pb-2 h-full overflow-hidden">
 				<div className="h-full grid grid-cols-[auto_1fr] gap-2">
-					<div className="bg-yellow-400 w-72 rounded-md">
-						LIBRARY PANEL
+					<div className="bg-spotify-black w-80 rounded-md">
+						<LibraryPanel />
 					</div>
 					<div className="bg-spotify-black rounded-md overflow-y-scroll">
 						{children}

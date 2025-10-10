a8cbbbbd59e934a64445e74b3477a688733c6b09
James Michael <jomichael1994@gmail.com>
2025-10-10 16:40:24 +0100

    show total liked songs number in playlists tab

--- components/library/LibraryPanel/LibraryPlaylists/index.tsx
+++ components/library/LibraryPanel/LibraryPlaylists/index.tsx
@@ -6,32 +6,39 @@ import ListItem from '../ListItem';
 import { useAppSelector } from '@/redux/hooks';
 
 const LibraryPlaylists = () => {
-	const { playlists } = useAppSelector((state) => state.library);
+	const { playlists, likedSongs } = useAppSelector((state) => state.library);
+
+	const totalLikedSongs = likedSongs.pagination?.total;
 
 	return playlists.isLoading ? (
 		<Loader />
 	) : (
 		<div className="flex flex-col pb-3">
-			{playlists.items.map((playlist) => (
-				<ListItem
-					key={playlist.id}
-					href={
-						playlist.id === 'liked-songs'
-							? '/liked-songs'
-							: `/playlist/${playlist.id}`
-					}
-					imageUrl={playlist.images[0]?.url}
-					name={playlist.name}
-					isPinned={
-						'isPinned' in playlist ? playlist.isPinned : false
-					}
-					secondaryText={
-						'owner' in playlist
-							? playlist.owner.display_name
-							: undefined
-					}
-				/>
-			))}
+			{playlists.items.map((playlist) => {
+				const isLikedSongs = playlist.id === 'liked-songs';
+				const secondaryText =
+					isLikedSongs && totalLikedSongs
+						? `${totalLikedSongs.toLocaleString()} songs`
+						: 'owner' in playlist
+						? playlist.owner.display_name
+						: undefined;
+				return (
+					<ListItem
+						key={playlist.id}
+						href={
+							playlist.id === 'liked-songs'
+								? '/liked-songs'
+								: `/playlist/${playlist.id}`
+						}
+						imageUrl={playlist.images[0]?.url}
+						name={playlist.name}
+						isPinned={
+							'isPinned' in playlist ? playlist.isPinned : false
+						}
+						secondaryText={secondaryText}
+					/>
+				);
+			})}
 		</div>
 	);
 };

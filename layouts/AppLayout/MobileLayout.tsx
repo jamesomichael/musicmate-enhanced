ff7f9f646073d1a4cc11f405655377fa10ba436b
James Michael <jomichael1994@gmail.com>
2025-10-11 17:49:45 +0100

    update mobile layout placeholder

--- layouts/AppLayout/MobileLayout.tsx
+++ layouts/AppLayout/MobileLayout.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const MobileLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="flex flex-col h-full">
-			<div className="bg-orange-500 h-full">{children}</div>
+			<div className="h-full flex justify-center items-center">
+				<span className="font-funnel text-spotify-green font-bold text-xl">
+					Coming soon...
+				</span>
+			</div>
+			{/* <div className="bg-orange-500 h-full">{children}</div> */}
 		</div>
 	);
 };

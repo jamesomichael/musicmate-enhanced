bc127279f4069b36534c5a595420b4552cfed392
James Michael <jomichael1994@gmail.com>
2025-10-12 20:04:05 +0100

    add CollectionTracklist and CollectionTracklistItem components

---
+++ components/collection/CollectionTracklist/CollectionTracklistItem/index.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import Link from 'next/link';
+import dayjs from 'dayjs';
+
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
+
+import type { SpotifyAlbumType, SpotifyTrack } from '@/types/spotify';
+
+type Props =
+	| {
+			type: 'playlist';
+			gridConfig: string;
+			number: number;
+			track: SpotifyTrack;
+			addedAt: string;
+	  }
+	| {
+			type: SpotifyAlbumType;
+			gridConfig: string;
+			number: number;
+			track: SpotifyTrack;
+			addedAt?: never;
+	  };
+
+const CollectionTracklistItem = ({
+	type,
+	gridConfig = 'grid grid-cols-[2rem_1.5fr_5rem] gap-5',
+	number,
+	track,
+	addedAt,
+}: Props) => {
+	let formattedAddedAt;
+
+	if (type === 'playlist') {
+		const weeksAgo = dayjs().diff(dayjs(addedAt), 'weeks');
+		formattedAddedAt =
+			weeksAgo > 4
+				? dayjs(addedAt).format('D MMM YYYY')
+				: weeksAgo >= 1
+				? `${weeksAgo} week${weeksAgo > 1 ? 's' : ''} ago`
+				: dayjs(addedAt).fromNow();
+	}
+
+	return (
+		<div
+			className={`group h-16 px-4 py-2 ${gridConfig} hover:bg-[#ffffff1a] rounded-md items-center font-funnel`}
+		>
+			<span className="mr-1 text-right text-neutral-400 group-hover:text-white">
+				{number}
+			</span>
+			<div className="flex items-center gap-2.5 h-full truncate">
+				{type === 'playlist' && (
+					<div
+						className="bg-center bg-cover h-full aspect-square rounded"
+						style={{
+							backgroundImage: `url(${track.album.images?.[0]?.url})`,
+						}}
+					></div>
+				)}
+				<div className="flex flex-col justify-center truncate">
+					<span className="truncate">{track.name}</span>
+					<div className="flex items-center">
+						{track.explicit && (
+							<div className="mr-1 flex justify-center items-center h-4 aspect-square bg-neutral-300 rounded-xs">
+								<span className="font-bold text-[0.7rem] text-spotify-black">
+									E
+								</span>
+							</div>
+						)}
+						{track.artists.map((artist, idx) => (
+							<div
+								key={artist.id}
+								className={`text-sm text-neutral-400 group-hover:text-white ${
+									idx < track.artists.length - 1
+										? "after:content-[','] after:mr-1"
+										: ''
+								}`}
+							>
+								<Link
+									href={`/artist/${artist.id}`}
+									className="hover:underline"
+								>
+									{artist.name}
+								</Link>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+			{type === 'playlist' && (
+				<>
+					<Link
+						href={`/album/${track.album.id}`}
+						className="hover:underline truncate text-neutral-400 group-hover:text-white"
+					>
+						{track.album.name}
+					</Link>
+					<span className="text-neutral-300">{formattedAddedAt}</span>
+				</>
+			)}
+			<span className="text-right text-neutral-400">
+				{dayjs
+					.duration(track.duration_ms, 'milliseconds')
+					.format('m:ss')}
+			</span>
+		</div>
+	);
+};
+
+export default CollectionTracklistItem;
---
+++ components/collection/CollectionTracklist/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import { FaRegClock } from 'react-icons/fa6';
+
+import CollectionTracklistItem from './CollectionTracklistItem';
+
+import type {
+	SpotifyAlbumType,
+	SpotifyLibraryLikedSong,
+	SpotifyPlaylistItem,
+	SpotifyTrack,
+} from '@/types/spotify';
+import type { Pagination } from '@/types/library';
+
+type Props =
+	| {
+			type: 'playlist';
+			tracks: SpotifyPlaylistItem[] | SpotifyLibraryLikedSong[];
+			paginationData: Pagination;
+	  }
+	| {
+			type: SpotifyAlbumType;
+			tracks: SpotifyTrack[];
+			paginationData: Pagination;
+	  };
+
+const CollectionTracklist = ({ type, paginationData, tracks }: Props) => {
+	const gridConfig =
+		type === 'playlist'
+			? 'grid grid-cols-[2rem_1.5fr_1fr_1fr_5rem] gap-5'
+			: 'grid grid-cols-[2rem_1.5fr_5rem] gap-5';
+	return (
+		<div className="text-white p-8 divide-y divide-neutral-700 rounded-full">
+			<div
+				className={`${gridConfig} items-end px-4 pb-3 font-funnel text-neutral-400 text-sm`}
+			>
+				<span className="text-right mr-1">#</span>
+				<span>Title</span>
+				{type === 'playlist' && (
+					<>
+						<span>Album</span>
+						<span>Date added</span>
+					</>
+				)}
+				<div className="flex justify-end items-center">
+					<FaRegClock className="w-4 h-4" />
+				</div>
+			</div>
+
+			<div className="mt-4 flex flex-col">
+				{type === 'playlist'
+					? tracks.map(({ track, added_at }, idx) => (
+							<CollectionTracklistItem
+								key={`${idx}_${track.id}_${added_at}`}
+								addedAt={added_at}
+								gridConfig={gridConfig}
+								number={idx + 1}
+								track={track}
+								type={type}
+							/>
+					  ))
+					: tracks.map((track) => (
+							<CollectionTracklistItem
+								key={track.id}
+								gridConfig={gridConfig}
+								number={track.track_number}
+								track={track}
+								type={type}
+							/>
+					  ))}
+			</div>
+		</div>
+	);
+};
+
+export default CollectionTracklist;

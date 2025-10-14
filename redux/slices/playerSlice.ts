a0575f8d9f4383610fc39953ba25092f6cda75ce
James Michael <jomichael1994@gmail.com>
2025-10-14 17:16:08 +0100

    update player redux slice

--- redux/slices/playerSlice.ts
+++ redux/slices/playerSlice.ts
@@ -5,18 +5,14 @@ import {
 } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const initialState = {
+import type { PlayerState } from '@/types/player';
+
+const initialState: PlayerState = {
 	deviceId: null,
 	isReady: false,
 	playbackState: null,
 	isLoading: false,
 };
-
-export const play = createAsyncThunk('player/play', async () => {
-	const response = await axios.get('/api/play');
-	const data = response.data;
-	return data;
-});
 
 export const fetchPlaybackState = createAsyncThunk(
 	'player/fetchPlaybackState',
@@ -24,6 +20,24 @@ export const fetchPlaybackState = createAsyncThunk(
 		const response = await axios.get('/api/player/state');
 		const data = response.data;
 		return data;
+	}
+);
+
+export const play = createAsyncThunk(
+	'player/play',
+	async ({
+		deviceId,
+		contextUri,
+		offset,
+	}: {
+		deviceId: string;
+		contextUri: string;
+		offset: { uri: string };
+	}) => {
+		await axios.put(`/api/player/play?device_id=${deviceId}`, {
+			contextUri,
+			offset,
+		});
 	}
 );
 
@@ -36,6 +50,30 @@ const playerSlice = createSlice({
 		},
 		setIsReady(state, action) {
 			state.isReady = action.payload;
+		},
+		setPlaybackState(state, action) {
+			if (!state.playbackState) {
+				state.playbackState = {
+					item: action.payload.track,
+					device: {
+						id: state.deviceId!,
+						is_active: true,
+					},
+					progress_ms: action.payload.progress || 0,
+					is_playing: true,
+					timestamp: +new Date(),
+				};
+			} else {
+				state.playbackState.item = action.payload.track;
+				state.playbackState.progress_ms = action.payload.progress || 0;
+				state.playbackState.timestamp = +new Date();
+			}
+		},
+		setProgress(state, action) {
+			if (!state.playbackState) {
+				return;
+			}
+			state.playbackState.progress_ms = action.payload;
 		},
 		// setPlayer(state, action) {
 		// 	console.error('action', action);
@@ -56,20 +94,24 @@ const playerSlice = createSlice({
 export const getNowPlaying = createSelector(
 	[(state) => state.player.deviceId, (state) => state.player.playbackState],
 	(deviceId, playbackState) => {
-		if (!playbackState) {
+		if (!!!playbackState) {
 			return { isPlaying: false };
 		}
 		return {
 			isPlaying: playbackState.is_playing,
+			isActive: playbackState.device.is_active,
 			isExternal: playbackState.device.id !== deviceId,
+			isPrivate: playbackState.device.is_private_session,
 			isPodcast: playbackState.currently_playing_type === 'episode',
 			device: playbackState.device,
 			progress: playbackState.progress_ms,
+			timestamp: playbackState.timestamp,
 			item: playbackState.item,
 			context: playbackState.context,
 		};
 	}
 );
 
-export const { setDeviceId, setIsReady } = playerSlice.actions;
+export const { setDeviceId, setIsReady, setPlaybackState, setProgress } =
+	playerSlice.actions;
 export default playerSlice.reducer;

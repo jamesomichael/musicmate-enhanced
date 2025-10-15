a2c187d2cd01cfdeb6c166c9880874bf61513267
James Michael <jomichael1994@gmail.com>
2025-10-15 22:32:29 +0100

    update homepage placeholder

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,18 +1,9 @@
-'use client';
 import Loader from '@/components/shared/Loader';
-import Button from '@/components/shared/Button';
-
-import useLogOut from '@/hooks/useLogOut';
 
 export default function Home() {
-	const logOut = useLogOut();
-
 	return (
 		<div className="h-full">
-			<div className="h-44">
-				<Loader />
-			</div>
-			<Button onClick={logOut} text="Log out" className="my-2" />
+			<Loader />
 		</div>
 	);
 }

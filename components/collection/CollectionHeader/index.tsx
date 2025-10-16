e3fd8a09f39dba5caeaeeecdfa358799e7dc6b35
James Michael <jomichael1994@gmail.com>
2025-10-16 22:13:31 +0100

    remove extra div in CollectionHeader

--- components/collection/CollectionHeader/index.tsx
+++ components/collection/CollectionHeader/index.tsx
@@ -34,72 +34,68 @@ const CollectionHeader = ({
 		<div
 			className={`flex flex-col bg-gradient-to-b ${gradientFrom} ${gradientTo}`}
 		>
-			<div className="grid grid-rows-[auto_1fr]">
-				<div className="h-72 grid grid-cols-[auto_1fr] gap-6 p-8">
-					{imageUrl ? (
-						<div
-							className="bg-cover bg-center h-full aspect-square rounded-md"
-							style={{
-								backgroundImage: `url(${imageUrl})`,
-							}}
-						></div>
-					) : (
-						<div className="h-full flex justify-center items-center aspect-square rounded-md bg-neutral-800">
-							<IoIosMusicalNotes className="w-28 h-28 text-neutral-400" />
-						</div>
-					)}
-					<div className="flex flex-col justify-end gap-2 overflow-auto">
-						<span className="capitalize leading-none font-funnel text-xs text-white">
-							{type}
-						</span>
-						<span className="font-unbounded font-black text-white text-4xl xl:leading-16 xl:text-[3.25rem] line-clamp-2">
-							{title}
-						</span>
-						{description && (
-							<span className="font-funnel text-sm text-neutral-300">
-								<span className="leading-5 font-medium line-clamp-2">
-									{he.decode(description)}
-								</span>
+			<div className="h-72 grid grid-cols-[auto_1fr] gap-6 p-8">
+				{imageUrl ? (
+					<div
+						className="bg-cover bg-center h-full aspect-square rounded-md"
+						style={{
+							backgroundImage: `url(${imageUrl})`,
+						}}
+					></div>
+				) : (
+					<div className="h-full flex justify-center items-center aspect-square rounded-md bg-neutral-800">
+						<IoIosMusicalNotes className="w-28 h-28 text-neutral-400" />
+					</div>
+				)}
+				<div className="flex flex-col justify-end gap-2 overflow-auto">
+					<span className="capitalize leading-none font-funnel text-xs text-white">
+						{type}
+					</span>
+					<span className="font-unbounded font-black text-white text-4xl xl:leading-16 xl:text-[3.25rem] line-clamp-2">
+						{title}
+					</span>
+					{description && (
+						<span className="font-funnel text-sm text-neutral-300">
+							<span className="leading-5 font-medium line-clamp-2">
+								{he.decode(description)}
 							</span>
-						)}
-						<div className="font-funnel text-sm flex items-center flex-wrap">
-							{creators.map((creator, idx) => (
-								<div
-									key={creator.id}
-									className={`font-bold text-white ${
-										idx > 0
-											? "before:content-['•'] before:mx-1"
-											: ''
-									}`}
+						</span>
+					)}
+					<div className="font-funnel text-sm flex items-center flex-wrap">
+						{creators.map((creator, idx) => (
+							<div
+								key={creator.id}
+								className={`font-bold text-white ${
+									idx > 0
+										? "before:content-['•'] before:mx-1"
+										: ''
+								}`}
+							>
+								<Link
+									href={
+										type === 'playlist'
+											? `/user/${creator.id}`
+											: `/artist/${creator.id}`
+									}
+									className="hover:underline"
 								>
-									<Link
-										href={
-											type === 'playlist'
-												? `/user/${creator.id}`
-												: `/artist/${creator.id}`
-										}
-										className="hover:underline"
-									>
-										{creator.name}
-									</Link>
-								</div>
-							))}
-							{totalFollowers && totalFollowers > 0 && (
-								<div className="before:content-['•'] before:mx-1 text-neutral-300">
-									<span>
-										{totalFollowers.toLocaleString()}{' '}
-										{totalFollowers === 1
-											? 'save'
-											: 'saves'}
-									</span>
-								</div>
-							)}
+									{creator.name}
+								</Link>
+							</div>
+						))}
+						{totalFollowers && totalFollowers > 0 && (
 							<div className="before:content-['•'] before:mx-1 text-neutral-300">
 								<span>
-									{totalTracks.toLocaleString()}{' '}
-									{totalTracks === 1 ? 'song' : 'songs'}
+									{totalFollowers.toLocaleString()}{' '}
+									{totalFollowers === 1 ? 'save' : 'saves'}
 								</span>
 							</div>
+						)}
+						<div className="before:content-['•'] before:mx-1 text-neutral-300">
+							<span>
+								{totalTracks.toLocaleString()}{' '}
+								{totalTracks === 1 ? 'song' : 'songs'}
+							</span>
 						</div>
 					</div>
 				</div>

a146759d454b8a3e5bb30e7240ce167c6a38fc54
James Michael <jomichael1994@gmail.com>
2025-11-28 02:02:32 +0000

    show non-premium subscription status in navbar

--- components/navbar/Navbar/index.tsx
+++ components/navbar/Navbar/index.tsx
@@ -8,8 +8,13 @@ import AccountIcon from '../AccountIcon';
 
 import { GoHomeFill, GoHome } from 'react-icons/go';
 import { BiSolidCategory, BiCategory } from 'react-icons/bi';
+import { FaSpotify } from 'react-icons/fa6';
+
+import { useAppSelector } from '@/redux/hooks';
+import { isUserPremium } from '@/redux/slices/userSlice';
 
 const Navbar = () => {
+	const userHasPremium = useAppSelector(isUserPremium);
 	return (
 		<div className="px-2 py-1.5 h-full grid grid-cols-[1fr_2fr_1fr] items-center">
 			<div>
@@ -32,7 +37,15 @@ const Navbar = () => {
 					iconSize={22}
 				/>
 			</div>
-			<div className="h-full flex justify-end items-center">
+			<div className="h-full flex gap-2 justify-end items-center">
+				{!userHasPremium && (
+					<div className="flex justify-center items-center gap-1.5 bg-[#C0C0C0] text-neutral-950 py-2 px-3.5 rounded-full">
+						<FaSpotify className="h-5 w-5" />
+						<span className="capitalize font-funnel font-medium text-sm">
+							Spotify Free
+						</span>
+					</div>
+				)}
 				<AccountIcon />
 			</div>
 		</div>

70302ecf602495975bb29390446a38454c89e202
James Michael <jomichael1994@gmail.com>
2025-11-29 01:10:35 +0000

    show recently played, top artists, and top tracks on homepage

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,11 +1,40 @@
-import Loader from '@/components/shared/Loader';
-import SuggestionsGrid from '@/components/suggestions/SuggestionsGrid';
+import { cookies } from 'next/headers';
 
-export default function Home() {
+import SuggestionsGrid from '@/components/suggestions/SuggestionsGrid';
+import UserTopTracks from '@/components/user/UserTopTracks';
+import UserTopArtists from '@/components/user/UserTopArtists';
+import RecentlyPlayed from '@/components/user/RecentlyPlayed';
+
+import {
+	fetchUserRecentlyPlayed,
+	fetchUserTopArtists,
+	fetchUserTopTracks,
+} from '@/services/spotify';
+
+const Home = async () => {
+	const cookieStore = await cookies();
+	const accessToken = cookieStore.get('access_token')!.value;
+
+	const { items: topTracks } = await fetchUserTopTracks({}, accessToken);
+	const { items: topArtists } = await fetchUserTopArtists({}, accessToken);
+	const { items: recentlyPlayedTracks } = await fetchUserRecentlyPlayed(
+		accessToken
+	);
+
 	return (
-		<div className="h-full">
+		<div className="h-full flex flex-col gap-4">
 			<SuggestionsGrid />
-			<Loader />
+			<div className="flex flex-col gap-4 px-6 pb-6">
+				{recentlyPlayedTracks?.length > 0 && (
+					<RecentlyPlayed tracks={recentlyPlayedTracks} />
+				)}
+				{topArtists?.length > 0 && (
+					<UserTopArtists artists={topArtists} />
+				)}
+				{topTracks?.length > 0 && <UserTopTracks tracks={topTracks} />}
+			</div>
 		</div>
 	);
-}
+};
+
+export default Home;
